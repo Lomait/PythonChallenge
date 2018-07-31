a906484a50f47fdee2c270d36ce6cc39bb43425b
Lomait <32894674+Lomait@users.noreply.github.com>
2018-07-31 18:08:13 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+
+# coding: utf-8
+
+# In[15]:
+
+a=[1,2,3,4]
+a1=[s*s for s in a]
+print a1
+
---
+++ 2..py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[1]:
+
+l1=[10, 15, 20, 12, 25, 45, 18]
+l2=[4, 5, 6, 3, 2, 9]
+for k in l1:
+    for j in l2:
+        if k % j ==0:
+            
+            print j, "is a factor of ", k
+
+
+# In[ ]:
+
+
+
